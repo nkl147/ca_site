1b2bf0404196d96d126a640c8a2fe65d0ec496bc
Ankit Sharma <ankitsharma1141@gmail.com>
2018-11-08 15:56:18 +0530

    modified for deployment

--- casite/settings.py
+++ casite/settings.py
@@ -139,3 +139,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+# For deployment
+try:
+    from .local_settings import *
+except ImportError:
+    pass
